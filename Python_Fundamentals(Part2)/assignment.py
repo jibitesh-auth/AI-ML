777c5b1828bb936300082f8576fe024b21e1dd1c
jibitesh-auth <jibiteshkumarmishra8027@gmail.com>
2026-07-08 17:34:10 +0530

    08-07-26

--- Python_Fundamentals(Part2)/assignment.py
+++ Python_Fundamentals(Part2)/assignment.py
@@ -2,7 +2,7 @@
 # salary = float(input("Enter the salary: "))
 # if salary < 30000:
 #     tax = salary * 0.05
-# elif salary >= 30000 and salary < 70000:
+# elif salary <= 70000:
 #     tax = salary * 0.15
 # else:
 #     tax = salary * 0.25
@@ -20,14 +20,105 @@
 # even_num(a,b)
 
 #3
-# def right_print(n):
-#     a = ''
+# def digits(n):
 #     while n>0:
-#         last_digit = n % 10
-#         a+=str(last_digit)
+#         last_digit = n%10
+#         print(last_digit)
 #         n//=10
-#     for i in range(len(a)-1, -1, -1):
-#         print(a[i])
     
+# digits(312)
 
-# right_print(312)
+#4
+# def count(n):
+#     c = 0
+#     while n>0:
+#         last_digit =n%10
+#         n//=10
+#         c+=1
+#     return c
+
+# n = int(input("Enter the no: "))
+# print(count(n))
+
+#5
+# def sum(n):
+#     s = 0
+#     while n>0:
+#         last_digit =n%10
+#         s+=last_digit
+#         n//=10
+        
+#     return s
+
+# n = int(input("Enter the no: "))
+# print(sum(n))
+
+#6
+# for i in range(1,101):
+#     if i%3==0 and i%5==0:
+#         print(i)
+
+#OR
+
+#for i in range(15,101,15):
+#   print(i)
+
+#7
+# while True:
+#     a = input("Enter the no: ")
+#     if a == 'Quit':
+#         break
+#     elif int(n) > 0:
+#          print("Positive")
+#     elif int(n) < 0:
+#          print("Negative")
+#     else:
+#          print("Wrong Input")
+
+#8
+# def calculator(a,b,operation):
+#     match operation:
+#         case '+':
+#             return a+b
+#         case '-':
+#             return a-b
+#         case '*':
+#             return a*b
+#         case '/':
+#             return a/b
+#         case _:
+#             return "Invalid value"
+        
+# a = int(input("Enter the first value: "))
+# b = int(input("Enter the second value: "))
+# c = input("Enter the Operation: ")
+# print(calculator(a,b,c))
+
+#9
+# def is_prime(n):
+#     if n<2:
+#         return False
+#     if n==2:
+#         return True
+#     for i in range(2,n):
+#         if n%i == 0:
+#             return False
+    
+#     return True
+
+
+# n = int(input("Enter the no: "))
+# print(is_prime(n))
+
+#10
+# secret_no = 10
+# a = int(input("Guess the no: "))
+# if a>secret_no:
+#     print("Too high")
+# elif a<secret_no:
+#     print("Too low")
+# else:
+#     print("Correct!")
+
+
+   
--- Python_Fundamentals(Part2)/tempCodeRunnerFile.py
+++ Python_Fundamentals(Part2)/tempCodeRunnerFile.py
@@ -1,9 +1,6 @@
-salary = float(input("Enter the salary: "))
-# if salary < 30000:
-#     tax = salary * 0.05
-# elif salary >= 30000 and salary < 70000:
-#     tax = salary * 0.15
-# else:
-#     tax = salary * 0.25
-
-# print(tax)
+while True:
+#     a = input("Enter the no: ")
+#     if a == 'Quit':
+#         break
+#     else:
+#          print(int(a))
