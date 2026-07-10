53237b5f846c37857e8c61d1122b48de1efd4610
jibitesh-auth <jibiteshkumarmishra8027@gmail.com>
2026-07-10 17:54:06 +0530

    10-07-26

---
+++ Python_Fundamentals(Part-3)/string.py
@@ -0,0 +1,52 @@
+# word = "python"
+# print(len(word))
+
+# word1 = "I love"
+# word2 = "python"
+# sentence = word1 + " " + word2
+
+#concatenate
+# print(word1 + " " + word2)
+# print(sentence)
+
+#-------------------x------------------------------
+
+# word = "Python"
+# print(word[2])
+
+# for ch in word:
+#     print(ch)
+
+# word[2] ='h'
+# print(word)
+
+#--------------------x---------------------------
+
+#Slicing
+
+# word = "Python"
+# print(word[2:4])
+ 
+# word = "I study from ApnaCollege"
+
+# print(word[13:25])
+# print(word[13:])
+
+# print(word[:])
+# print(word[:len(word)])
+
+#-------------------x------------------------------
+
+#negative indexing
+# word = "Python"
+# print(word[-4:-2])
+
+#------------------x------------------------
+
+#Formatting
+
+a = 5
+b = 10
+sum = a+b
+print("language is {}".format("python"))
+print("sum is {}".format(sum))
