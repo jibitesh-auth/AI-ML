518349aa4d2e1591cfdad13056dee8833ad926fa
jibitesh-auth <jibiteshkumarmishra8027@gmail.com>
2026-07-21 17:15:06 +0530

    21-07-26

--- Python_Fundamentals_1/variable.py
+++ Python_Fundamentals_1/variable.py
@@ -1,5 +1,10 @@
-_name = "Jibitesh"
-age = 21
-PI = 3.14
-print("My name is:",_name)
-print("My age is:", age+5)
+# _name = "Jibitesh"
+# age = 21
+# PI = 3.14
+# print("My name is:",_name)
+# print("My age is:", age+5)
+
+
+#Extra
+x = False
+print(~x)
