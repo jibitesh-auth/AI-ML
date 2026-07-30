e51ceea01f347affc2098a51ca16af74668b9ddb
jibitesh-auth <jibiteshkumarmishra8027@gmail.com>
2026-07-30 17:59:33 +0530

    30-07-26

---
+++ Python_Fundamentals(Part-4)/polymorphism.py
@@ -0,0 +1,19 @@
+#Function Overriding
+
+# class Employee:
+#     def get_designation(self):
+#         print("designation = Employee")
+
+# class Teacher(Employee):
+#     def get_designation(self):
+#         print("designation = Teacher")
+
+
+# t1 = Teacher()
+# t1.get_designation()
+
+
+#Duck Typing
+
+
+
