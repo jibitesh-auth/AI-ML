018ab5f9e4221e42786740b29bff68e1e9025241
jibitesh-auth <jibiteshkumarmishra8027@gmail.com>
2026-08-06 17:52:33 +0530

    06-08-26

--- Python_Fundamentals(Part-4)/assignment_problem.py
+++ Python_Fundamentals(Part-4)/assignment_problem.py
@@ -261,38 +261,93 @@
 
 #*10
 
-from abc import ABC, abstractmethod
 class Message:
-    def __init__(self,message):
-        self.message = message
+    message_counter = 1
 
-    @abstractmethod
-    def sending_message(self):
-        pass
+    def __init__(self,sender,content):
+        self.sender = sender
+        self.content = content
+        self.id = Message.message_counter
+        Message.message_counter+=1
+
+    def __str__(self):
+        return f"({self.id}) {self.sender.username} : {self.content}"
+
+class User:
+    def __init__(self,username):
+        self.username = username
+        self.chatroom = None
+
+    def join_chatroom(self,chatroom):
+        if self.chatroom:
+            print(f"{self.username} is already in a chatroom")
+        else:
+            chatroom.add_user(self)
+            self.chatroom = chatroom
+            print(f"{self.username} joined {chatroom.name}")
+
+    def leave_chatroom(self):
+        if not self.chatroom:
+            print(f"{self.username} is not in any chatroom")
+        else:
+            self.chatroom.remove_user(self)
+            print(f"{self.username} left {self.chatroom.name}")
+            self.chatroom = None
+
+    def send_message(self,content):
+        if not self.chatroom:
+            print(f"{self.chatroom} cannot send a message (not in a chatroom)")
+        else:
+            self.chatroom.broadcast(self,content)
+
 
 class ChatRoom:
-    @abstractmethod
-    def user_join_leave(self):
-        pass
-    def chat_history(self):
-        pass
-    
-
-
-class User(Message,ChatRoom):
-    list=[]
-    def __init__(self,name,message):
-        Message.__init__(self,message)
+    def __init__(self,name):
         self.name = name
+        self.users = []
+        self.messages = []
 
-    def sending_message(self):
-        self.message = self.message
+    def add_user(self,user):
+        self.users.append(user)
 
-    def chat_history(self):
-        User.list.append(self.message)
-        return list
+    def remove_user(self,user):
+        self.users.remove(user)
+
+    def broadcast(self,sender,content):
+        message = Message(sender,content)
+        self.messages.append(message)
+        print(message)
+
+    def show_chat_history(self):
+        print(f"\nChat History of {self.name}")
+        for msg in self.messages:
+            print(msg)
+
+        print()
+
+if __name__ == "main":
+    room = ChatRoom("Python Lounge")
+    u1 = User("Alice")
+    u2 = User("Bob")
+    u3 = User("Charlie")
+
+    u1.join_chatroom(room)
+    u2.join_chatroom(room)
+
+    u1.send_message("Hello Everyone")
+    u2.send_message("Hi Alice!")
+
+    u3.join_chatroom(room)
+    u3.send_message("Hi Alice")
+
+    room.show_chat_history()
+
+    u1.leave_chatroom()
+    u2.leave_chatroom()
+    u3.leave_chatroom()
+
+
     
-    def user_join_leave()
 
     
 
